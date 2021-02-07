77176e5b7ad6c3e11e02a7b8d63aaabca7586956
Yuting Zhang <yz579@duke.edu>
2021-02-07 02:00:30 +0000

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hi, this is the first project.'
+    return 'Hi, this is the first project. Do Test'
 
 @app.route('/name/<value>')
 def name(value):
